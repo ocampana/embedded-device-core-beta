2f131df7b209de9e052b2ddaca187e34b062eaa2
Ottavio Campana <ottavio@campana.vi.it>
2026-03-02 21:37:27 +0000

    backup, anche se dà segfault

--- src/monitor/control_loop.cc
+++ src/monitor/control_loop.cc
@@ -5,6 +5,11 @@
 
 #include <limits.h>
 
+#include <iostream>
+#include <iomanip>
+
+using namespace core;
+
 void control_loop (bool *exit_flag)
 {
     int input_fifo;
@@ -18,9 +23,17 @@ void control_loop (bool *exit_flag)
         char buffer[PIPE_BUF];
         size_t size;
 
+        memset (buffer, 0, PIPE_BUF);
+
         if (read_fifo (input_fifo, buffer, PIPE_BUF, &size))
         {
+            flatbuffers::Verifier verifier((uint8_t*) buffer, size);
+
+            const core::Message* message = core::GetMessage(buffer);
+            printf ("%p\n", message);
+            printf ("%s\n", message->return_path()->str());
         }
+exit(0);
     }
 
     close_fifo (INPUT_FIFO, input_fifo);
